566b7d04ea1786e11326f6efc5519030c0390508
aniruddha <aniruddhathakur76@gmail.com>
2024-06-19 20:54:37 +0530

    Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -238,25 +238,41 @@ function App() {
           transform: 'translateX(-40%)'
         }}>ABOUTME</section>
 
-        <div className='p-36 flex justify-center' style={{ backgroundColor: '#14121c', zIndex:1}}>
-          <div id='border-class' className='flex p-6' style={{zIndex: 1}}>
-          <div style={{ width: '290px', zIndex: 4 }}>
-            <img id='profile' src="./src/assets/images/profile/aniruddha.jpg" alt="aniruddha-thakur-profile" />
-          </div>
+        <div className='justify-center' style={{paddingTop: '9rem', backgroundColor: '#14121c', zIndex: 1 }}>
+          <div className='flex justify-center'>
+            <div id='border-class' className='p-6' style={{ zIndex: 1 }}>
+              <div className='flex'>
+                <div style={{ width: '290px', zIndex: 4 }}>
+                  <img id='profile' src="./src/assets/images/profile/aniruddha.jpg" alt="aniruddha-thakur-profile" />
+                </div>
 
-          <div className='text-white text-xl' style={{zIndex: 4, paddingLeft: '3rem', width: '500px', fontFamily: 'Roboto Condensed,Lato,Arial,sans-serif', fontWeight: 400}}>
-            <p>
-              Hello! I'm Aniruddha Thakur, a passionate web developer and designer who thrives on creating stunning and intuitive frontend designs.  
-              </p><br />
-              <p>
-              My journey in the digital world began with a fascination for how design and code can come together to create seamless user experiences.
-              </p>
-              <br />
-              <p>
-              As a web developer, I meticulously craft responsive and efficient code that brings my designs to life. 
-              My goal is to make every website not only look fantastic but also function flawlessly.
-            </p>
-          </div>
+                <div className='text-white text-xl' style={{ zIndex: 4, paddingLeft: '3rem', width: '500px', fontFamily: 'Roboto Condensed,Lato,Arial,sans-serif', fontWeight: 400 }}>
+                  <p>
+                    Hello! I'm Aniruddha Thakur, a passionate web developer and designer who thrives on creating stunning and intuitive frontend designs.
+                  </p><br />
+                  <p>
+                    My journey in the digital world began with a fascination for how design and code can come together to create seamless user experiences.
+                  </p>
+                  <br />
+                  <p>
+                    As a web developer, I meticulously craft responsive and efficient code that brings my designs to life.
+                    My goal is to make every website not only look fantastic but also function flawlessly.
+                  </p>
+                </div>
+              </div>
+
+              <div id='download-resume' className='py-4 flex justify-end' >
+                <button class="resume-btn" id="resume-btn" onClick={function() {window.open('https://drive.google.com/file/d/1KXSYTf9NOupshEV5N9_UjJTxIq_SwdJI/view?usp=sharing', '_blank')}}> 
+                  <div className="sign">
+                    <svg viewBox="0 0 640 512">
+                      <path d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-167l80 80c9.4 9.4 24.6 9.4 33.9 0l80-80c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-39 39V184c0-13.3-10.7-24-24-24s-24 10.7-24 24V318.1l-39-39c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9z"></path>
+                    </svg>
+                  </div>
+
+                  <div class="text">Resume</div>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
