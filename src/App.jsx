dc76be081f25601f8488a2c3f92c81172e122dbc
aniruddha <aniruddhathakur76@gmail.com>
2024-06-23 01:18:12 +0530

    Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,16 @@ function App() {
         <div className="b-center bg-no-repeat bg-cover min-h-dvh" style={{ backgroundColor: "#11111b" }}>
           <div className='hidden sm:block blob h-[500px] w-[600px]'></div>
           <div className='min-h-dvh px-10 sm:px-16 md:px-60 start-container name' style={{ paddingTop: "6rem", paddingBottom: 0 }}>
-            <div className='text-white text-2xl sm:text-4xl' style={{ fontFamily: "Roboto Condensed,Lato,Arial,sans-serif" }}>
+            <div className='text-white text-xl sm:text-4xl' style={{ fontFamily: "Roboto Condensed,Lato,Arial,sans-serif" }}>
               <h1>Hey there!, I'm</h1>
-              <div className='pt-5 pb-10 text-[3rem] sm:text-[4rem] md:text-[5rem]' style={{ fontWeight: 700 }}>
+              <div className='pt-5 pb-5 sm:pb-10 text-[2rem] sm:text-[4rem] md:text-[5rem] text-[#5918df]' style={{ fontWeight: 700 }}>
                 Aniruddha Thakur
               </div>
-              <div className='text-[3rem] sm:text-[4rem] md:text-[5rem]' style={{fontWeight: 700 }}>
+              <div className='text-[2rem] sm:text-[4rem] md:text-[5rem]' style={{fontWeight: 700 }}>
                 I Design & Code For Web
               </div>
 
-              <div className='text-xl sm:text-2xl py-10'>
+              <div className='text-xl sm:text-2xl py-5 sm:py-10'>
                 I'm a web developer who loves crafting digital magic through
                 vibrant concept art and sleek frontend designs. I Love fun Web UI, collaboration and turning idea's into reality.<br />
                 <br />
@@ -58,7 +58,7 @@ function App() {
           </div>
 
           <div className="relative sm:flex py-14 justify-end">
-            <div className="relative px-14" style={{ paddingRight: "25rem" }}>
+            <div className="relative px-14 sm:pr-[25rem]">
               <img src="./src/assets/images/instadownloader.png" alt="studiosrepo" className="h-52 w-99 brightness-75" style={{ boxShadow: "0 -4px 100px rgba(0, 0, 0, 0.5)" }} />
               <div className="absolute bottom-0 left-0 p-4 text-white" style={{ fontSize: '22px', fontFamily: 'Roboto Condensed,Lato,Arial,sans-serif', fontWeight: 700, textShadow: '2px 2px 4px rgba(0, 0, 0, 0.6)' }}>
                 <p>Insta<br />Downloader</p>
@@ -98,7 +98,7 @@ function App() {
           </div>
 
           <div className='sm:px-40 py-5'>
-            <div className="relative px-14" style={{ paddingRight: "25rem" }}>
+            <div className="relative sm:px-14 sm:pr-[25rem]">
               <img src="./src/assets/images/instadownloader.png" alt="studiosrepo" className="h-52 w-99 brightness-75" style={{ boxShadow: "0 -4px 100px rgba(0, 0, 0, 0.5)" }} />
               <div className="absolute bottom-0 left-0 p-4 text-white" style={{ fontSize: '22px', fontFamily: 'Roboto Condensed,Lato,Arial,sans-serif', fontWeight: 700, textShadow: '2px 2px 4px rgba(0, 0, 0, 0.6)' }}>
                 <p>Blockify<br />Chrome Extension</p>
@@ -110,17 +110,17 @@ function App() {
         </div>
 
         <div id='tech-stack'>
-          <section id='techstack' className='pl-[40%] sm:pl-[50%] md:pl-[100%] text-[80px] sm:text-[100px] md:text-[140px]'>TECHSTACK</section>
+          <section id='techstack' className='pl-[100%] sm:pl-[50%] md:pl-[100%] text-[50px] sm:text-[100px] md:text-[140px]'>TECHSTACK</section>
 
-          <div className='flex justify-center pt-[80px] sm:pt-[140px]'>
-            <div id='tech-stack' className='w-[20rem] sm:w-[60rem]' style={{ zIndex: 5, boxShadow: "0 -4px 100px rgba(0, 0, 0, 0.5)", height: "auto", backgroundImage: "linear-gradient(-180deg, #1a1527, #0e0c16 88%, #0e0c16 99%)" }}>
-              <div id='row-1' className='flex flex-col sm:flex-row justify-evenly py-14'>
-                <div id='image-container' className='tooltip p-5 h-40 w-40 sm:h-40 sm:w-40'>
+          <div className='flex justify-center pt-[50px] sm:pt-[140px]'>
+            <div id='tech-stack' className='w-[30rem] sm:w-[60rem]' style={{ zIndex: 5, boxShadow: "0 -4px 100px rgba(0, 0, 0, 0.5)", height: "auto", backgroundImage: "linear-gradient(-180deg, #1a1527, #0e0c16 88%, #0e0c16 99%)" }}>
+              <div id='row-1' className='p-5 sm:p-0 sm:flex sm:flex-row justify-evenly sm:py-14'>
+                <div id='image-container' className='tooltip p-5 h-40 w-40'>
                   <img src="./src/assets/images/logo/html.png" />
                   <span className='tooltiptext'>HTML</span>
                 </div>
 
-                <div id='image-container' className='tooltip h-40 w-40 p-5'>
+                <div id='image-container' className='tooltip p-5 h-40 w-40'>
                   <img src="./src/assets/images/logo/css.png" />
                   <span className='tooltiptext'>CSS</span>
                 </div>
@@ -136,7 +136,7 @@ function App() {
                 </div>
               </div>
 
-              <div id='rwo-2' className='flex flex-col sm:flex-row justify-evenly' style={{ paddingBottom: '3.5rem' }}>
+              <div id='rwo-2' className='p-5 sm:p-0 sm:flex sm:flex-row justify-evenly' style={{ paddingBottom: '3.5rem' }}>
                 <div id='image-container' className='tooltip h-40 w-40 p-5'>
                   <img src="./src/assets/images/logo/reactlogo.png" />
                   <span className='tooltiptext'>REACT</span>
@@ -158,7 +158,7 @@ function App() {
                 </div>
               </div>
 
-              <div id='rwo-3' className='flex flex-col sm:flex-row justify-evenly' style={{ paddingBottom: '3.5rem' }}>
+              <div id='rwo-3' className='p-5 sm:p-0 sm:flex sm:flex-row justify-evenly' style={{ paddingBottom: '3.5rem' }}>
                 <div id='image-container' className='tooltip h-40 w-40 p-5'>
                   <img src="./src/assets/images/logo/express.png" />
                   <span className='tooltiptext'>EXPRESS JS</span>
@@ -180,7 +180,7 @@ function App() {
                 </div>
               </div>
 
-              <div id='rwo-4' className='flex flex-col sm:flex-row justify-evenly' style={{ paddingBottom: '3.5rem' }}>
+              <div id='rwo-4' className='p-5 sm:p-0 sm:flex sm:flex-row justify-evenly' style={{ paddingBottom: '3.5rem' }}>
                 <div id='image-container' className='tooltip h-40 w-40 p-5'>
                   <img src="./src/assets/images/logo/discord.png" />
                   <span className='tooltiptext'>DISCORD JS</span>
@@ -202,7 +202,7 @@ function App() {
                 </div>
               </div>
 
-              <div id='rwo-5' className='flex flex-col sm:flex-row justify-evenly' style={{ paddingBottom: '3.5rem' }}>
+              <div id='rwo-5' className='p-5 sm:p-0 sm:flex sm:flex-row justify-evenly' style={{ paddingBottom: '3.5rem' }}>
                 <div id='image-container' className='tooltip h-40 w-40 p-5'>
                   <img src="./src/assets/images/logo/canvalogo.png" />
                   <span className='tooltiptext'>CANVA</span>
@@ -221,11 +221,11 @@ function App() {
           <div className='flex flex-col sm:flex-row justify-center'>
             <div className='p-6' style={{ zIndex: 1 }}>
               <div id='border-class' className='flex flex-col sm:flex-row p-4'>
-                <div style={{ width: '290px', zIndex: 4 }}>
+                <div className='sm:w-[290px] z=[4]'>
                   <img id='profile' src="./src/assets/images/profile/aniruddha.jpg" alt="aniruddha-thakur-profile" />
                 </div>
 
-                <div className='text-white text-xl sm:pl-[3rem] sm:w-[500px]' style={{ zIndex: 4, fontFamily: 'Roboto Condensed,Lato,Arial,sans-serif', fontWeight: 400 }}>
+                <div className='text-white text-xl pt-4 sm:pt-0 sm:pl-[3rem] sm:w-[500px]' style={{ zIndex: 4, fontFamily: 'Roboto Condensed,Lato,Arial,sans-serif', fontWeight: 400 }}>
                   <p>
                     Hello! I'm Aniruddha Thakur, a passionate web developer and designer who thrives on creating stunning and intuitive frontend designs.
                   </p><br />
@@ -260,45 +260,45 @@ function App() {
         <section id='contact-section' className='pl-[100%] sm:pl-[140%] md:pl-[155%] text-[80px] sm:text-[100px] md:text-[140px]'>CONTACT</section>
 
         <div id='socials' className='flex flex-col justify-end'>
-          <div className='flex justify-center gap-[10px] sm:gap-[30px] pt-[4rem] z-[1]'>
+          <div className='flex justify-center gap-[10px] sm:gap-[30px] px-[2rem] sm:px-0 pt-[4rem] z-[1]'>
 
-            <div className='icons instagram p-[16px]' onClick={() => window.open("https://www.instagram.com/aniiruddhaa_/", '_blank')}>
+            <div className='icons instagram p-[10px] sm:p-[16px] sm:w-[4.5%]' onClick={() => window.open("https://www.instagram.com/aniiruddhaa_/", '_blank')}>
               <img id="instagram" src="./src/assets/images/socials/instagram.png" alt="ani-instagram" />
             </div>
 
-            <div className='icons linkedin p-[16px]' onClick={() => window.open("https://www.linkedin.com/in/aniruddha-thakur-b36482259/", '_blank')}>
+            <div className='icons linkedin p-[10px] sm:p-[16px] sm:w-[4.5%]' onClick={() => window.open("https://www.linkedin.com/in/aniruddha-thakur-b36482259/", '_blank')}>
               <img src="./src/assets/images/socials/linkedin.png" alt="ani-linkedin" />
             </div>
 
-            <div className='icons github p-[16px]' onClick={() => window.open("https://github.com/aniruddha76", '_blank')}>
+            <div className='icons github p-[10px] sm:p-[16px] sm:w-[4.5%]' onClick={() => window.open("https://github.com/aniruddha76", '_blank')}>
               <img src="./src/assets/images/socials/github.png" alt="ani-github" />
             </div>
 
-            <div className='icons discord p-[16px]' onClick={() => window.open("https://discordapp.com/users/raptor1570", '_blank')}>
+            <div className='icons discord p-[10px] sm:p-[16px] sm:w-[4.5%]' onClick={() => window.open("https://discordapp.com/users/raptor1570", '_blank')}>
               <img src="./src/assets/images/socials/discord.png" alt="ani-discord" />
             </div>
 
-            <div className='icons email p-[16px]' onClick={() => open('mailto:aniruddhathakur76@gmail.com', '_blank')}>
+            <div className='icons email p-[10px] sm:p-[16px] sm:w-[4.5%]' onClick={() => open('mailto:aniruddhathakur76@gmail.com', '_blank')}>
               <img src="./src/assets/images/socials/email.png" alt="ani-email" />
             </div>
 
           </div>
         </div>
 
-        <div className='flex justify-center text-white py-4' style={{ fontFamily: 'Roboto Condensed,Lato,Arial,sans-serif' }}>
+        <div className='flex justify-center text-white' style={{ fontFamily: 'Roboto Condensed,Lato,Arial,sans-serif' }}>
           <div class="flex w-full items-center">
             <div class="mx-auto w-full max-w-lg form-container">
               <center><h1 class="text-4xl font-medium">Let's Connect</h1></center>
 
-              <form action="https://api.web3forms.com/submit" method='post' class="mt-10">
+              <form action="https://api.web3forms.com/submit" method='post' className="sm:mt-10">
 
                 <input type="hidden" name="access_key" value="d3cd45bf-0d18-4988-9798-934bb5293495" />
-                <div class="grid gap-6 sm:grid-cols-2">
-                  <div class="relative z-0">
+                <div class="flex flex-col justify-between p-8 sm:p-0 sm:grid sm:gap-6 sm:grid-cols-2">
+                  <div class="relative z-0 pb-4 sm:pb-0">
                     <input type="text" name="name" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm  focus:border-purple-600 focus:outline-none focus:ring-0" placeholder=" " />
                     <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-purple-600 peer-focus:dark:text-purple-500">Your name</label>
                   </div>
-                  <div class="relative z-0">
+                  <div class="relative z-0 pb-4 sm:pb-0">
                     <input type="text" name="email" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm focus:border-purple-600 focus:outline-none focus:ring-0" placeholder=" " />
                     <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-purple-600 peer-focus:dark:text-purple-500">Your email</label>
                   </div>
@@ -313,7 +313,7 @@ function App() {
           </div>
         </div>
 
-        <div className='footer h-24 flex justify-center items-center bg-black text-[#494949] font-medium'>
+        <div className='footer h-24 flex justify-center items-center bg-black text-[#494949] font-medium text-xs sm:text-base'>
           <p>Made with &lt;/&gt; by Aniruddha Thakur &copy; TwentyTwentyFour</p>
         </div>
 
--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,49 +1,115 @@
-import react, {useState} from "react"
+import React, { useState, useEffect } from "react";
 
 function Navbar() {
-
-    const[isMenuOpen, setMenuOpen] = useState(false);
+    const [isMenuOpen, setMenuOpen] = useState(false);
+    const [isScrollingUp, setScrollingUp] = useState(true);
 
     const toggleMenu = () => {
-        setMenuOpen(!isMenuOpen)
-    } 
+        setMenuOpen(!isMenuOpen);
+    };
+
+    const handleLinkClick = () => {
+        setMenuOpen(false);
+    };
+
+    let lastScrollTop = 0;
+
+    useEffect(() => {
+        const handleScroll = () => {
+            let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            if (scrollTop > lastScrollTop) {
+                // Downscroll code
+                setScrollingUp(false);
+            } else {
+                // Upscroll code
+                setScrollingUp(true);
+            }
+            lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
     return (
-        <nav id="main-nav" class="p-4 flex justify-between absolute top-0 w-full sm:w-full text-[22px] font-bold z-[999]" style={{ fontFamily: "Roboto Condensed,Lato,Arial,sans-serif" }}>
-
+        <nav id="main-nav" className={`p-4 flex justify-between fixed top-0 w-full sm:w-full text-[22px] font-bold z-[999] transition-transform duration-300 ${
+                isScrollingUp ? "transform translate-y-0" : "transform -translate-y-full"
+            }`} style={{ fontFamily: "Roboto Condensed,Lato,Arial,sans-serif" }}>
             <div className="text-white"></div>
 
             <div>
-                <ul className='hidden sm:flex text-white'>
-                    <li className='px-2'><a href='#start'>Start<span className='nav-span'> /&gt;</span></a></li>
-                    <li className='px-2'><a href='#work'>Projects<span className='nav-span'> /&gt;</span></a></li>
-                    <li className='px-2'><a href='#tech-stack'>Tech Stack<span className='nav-span'> /&gt;</span></a></li>
-                    <li className='px-2'><a href='#about'>About<span className='nav-span'> /&gt;</span></a></li>
-                    <li className='px-2'><a href='#contact'>Contact<span className='nav-span'> /&gt;</span></a></li>
+                <ul className="hidden sm:flex text-white">
+                    <li className="px-2">
+                        <a href="#start" onClick={handleLinkClick}>
+                            Start<span className="nav-span"> /&gt;</span>
+                        </a>
+                    </li>
+                    <li className="px-2">
+                        <a href="#work" onClick={handleLinkClick}>
+                            Projects<span className="nav-span"> /&gt;</span>
+                        </a>
+                    </li>
+                    <li className="px-2">
+                        <a href="#tech-stack" onClick={handleLinkClick}>
+                            Tech Stack<span className="nav-span"> /&gt;</span>
+                        </a>
+                    </li>
+                    <li className="px-2">
+                        <a href="#about" onClick={handleLinkClick}>
+                            About<span className="nav-span"> /&gt;</span>
+                        </a>
+                    </li>
+                    <li className="px-2">
+                        <a href="#contact" onClick={handleLinkClick}>
+                            Contact<span className="nav-span"> /&gt;</span>
+                        </a>
+                    </li>
                 </ul>
             </div>
 
-            <div className="hamburger flex flex-col sm:hidden justify-between w-[40px] h-[20px]" onClick={toggleMenu}>
-                <span className="bar bg-white  border-solid rounded border-2 w-[40%]"></span>
-                <span className="bar bg-white  border-solid rounded border-2 w-[80%]"></span>
-                <span className="bar bg-white  border-solid rounded border-2 w-[60%]"></span>     
+            <div className="hamburger flex flex-col sm:hidden justify-between w-[40px] h-[20px] z-[999]" onClick={toggleMenu}>
+                <span className={`bar bg-white border-solid rounded border-2 w-[40%] transition-transform duration-300 ${isMenuOpen ? 'transform rotate-45 translate-y-1' :''}`}></span>               
+                <span className={`bar bg-white border-solid rounded border-2 w-[80%] transition-transform duration-300 ${isMenuOpen ? 'transform -rotate-45 -translate-x-1' :''}`}></span>
+                <span className={`bar bg-white border-solid rounded border-2 w-[60%] transition-transform duration-300 ${isMenuOpen ? 'transform rotate-45 -translate-y-1 translate-x-1' :''}`}></span>
             </div>
 
-            <div
-                className={`mobile-menu w-full absolute bg-[#11111b] top-12 left-0 flex justify-center transition-all duration-300 ease-in-out ${
-                    isMenuOpen ? "max-h-[500px] p-5 shadow-xl" : "max-h-0 p-0"
-                } overflow-hidden`}
-            ><ul className='flex flex-col text-white items-center'>
-                    <li className='py-2'><a href='#start'>Start<span className='nav-span'> /&gt;</span></a></li>
-                    <li className='py-2'><a href='#work'>Projects<span className='nav-span'> /&gt;</span></a></li>
-                    <li className='py-2'><a href='#tech-stack'>Tech Stack<span className='nav-span'> /&gt;</span></a></li>
-                    <li className='py-2'><a href='#about'>About<span className='nav-span'> /&gt;</span></a></li>
-                    <li className='py-2'><a href='#contact'>Contact<span className='nav-span'> /&gt;</span></a></li>
+            <div className={`mobile-menu w-full absolute top-12 left-0 flex justify-center transition-all duration-300 ease-in-out ${
+                    isMenuOpen ? "max-h-[500px] p-5 shadow-[0 0 50px 80px #0e0c16]" : "max-h-0 p-0"
+                } overflow-hidden`} style={{ boxShadow: isMenuOpen ? "0 0 50px 80px #0e0c16" : "none" }}>
+                    
+                <ul className="flex flex-col text-white items-center">
+                    <li className="py-2">
+                        <a href="#start" onClick={handleLinkClick}>
+                            Start<span className="nav-span"> /&gt;</span>
+                        </a>
+                    </li>
+                    <li className="py-2">
+                        <a href="#work" onClick={handleLinkClick}>
+                            Projects<span className="nav-span"> /&gt;</span>
+                        </a>
+                    </li>
+                    <li className="py-2">
+                        <a href="#tech-stack" onClick={handleLinkClick}>
+                            Tech Stack<span className="nav-span"> /&gt;</span>
+                        </a>
+                    </li>
+                    <li className="py-2">
+                        <a href="#about" onClick={handleLinkClick}>
+                            About<span className="nav-span"> /&gt;</span>
+                        </a>
+                    </li>
+                    <li className="py-2">
+                        <a href="#contact" onClick={handleLinkClick}>
+                            Contact<span className="nav-span"> /&gt;</span>
+                        </a>
+                    </li>
                 </ul>
             </div>
-
         </nav>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
